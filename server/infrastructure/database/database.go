33929749d31f20bf33535a051d987fb80e73fe25
kuma033 <nhayato28@yahoo.co.jp>
2023-04-11 00:04:33 +0900

    configを直してinfraの中にNewDb関数を作った

--- server/infrastructure/database/database.go
+++ server/infrastructure/database/database.go
@@ -6,6 +6,21 @@ import (
 	"github.com/kuma0328/circle_board/config"
 )
 
+// データベースに接続して返す関数です
 func NewDB() (*sql.DB, error) {
-	dsn, driver := cofig.DSN()
+	dsn, driver := config.DSN()
+
+	// データベースに接続
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// データベースへの接続を確認
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
